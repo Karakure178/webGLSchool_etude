e47facb47b0294bbb0d3f51f810de37095d9764c
Karakure178 <egg9017shun@gmail.com>
2024-05-26 21:42:42 +0900

    fix - モデル更新他

--- 0524/src/three_2/index.js
+++ 0524/src/three_2/index.js
@@ -2,9 +2,10 @@ import * as THREE from 'three';
 import { gui_setup } from './guiBase.js';
 import { adjustment_deviceDisplay } from './three_libraries/renderer_setup.js';
 import { create_camera } from './three_libraries/camera_setup.js';
-import { createShader } from './createShader.js';
 import { map } from './three_libraries/map.js';
 import { radians } from './three_libraries/radians.js';
+import { gltfLoad } from './shape/gltfLoad.js';
+const meshLoad_frag = { bool: false };
 
 const init = () => {
   /* || 初期セットアップ */
@@ -19,60 +20,38 @@ const init = () => {
   /* || 初期セットアップ終わり */
 
   /* || メッシュの設定 */
-  const material = createShader(); // マテリアルの設定
-  const geometry = new THREE.BoxGeometry(1, 1, 1); // ジオメトリ設定
+  const material = new THREE.MeshNormalMaterial(); // マテリアル設定
+  const fan_path =
+    'https://raw.githubusercontent.com/Karakure178/webGLSchool_etude/main/0524/public/assets/image/senpuki_boolean.gltf';
+  gltfLoad(fan_path, scene, material, meshLoad_frag);
 
-  const meshs = [];
-  const num = 10;
-  const angle = 360 / num;
+  // https://observablehq.com/@kabokawakabo1/three-js-4-2
+  //scene.overrideMaterial = new THREE.MeshNormalMaterial();
 
-  for (let i = 0; i < num; i++) {
-    const mesh = new THREE.Mesh(geometry, material);
-    drawCircles(0, 0, 0, mesh, angle * i);
-    //scene.add(mesh);
-    meshs.push(mesh);
-  }
-  const group = new THREE.Group();
-  group.add(...meshs);
-  group.rotation.x = Math.PI / 2;
-  scene.add(group);
+  // 非同期で実行
+  // const mesh_1 = scene.children[0];
+
   /* || メッシュの設定終わり */
 
-  /* || ループ処理設定 */
   let clock = new THREE.Clock();
   function tick() {
     requestAnimationFrame(tick);
 
-    // アニメーション受け取り用シェーダーの値
-    //const uniforms = mesh.material.uniforms;
     // frameの受け取り
     //group.rotation.y += clock.getDelta();
     //uniforms.time.value += clock.getDelta();
+
+    // メッシュが読み込まれたら実行
+    if (meshLoad_frag.bool) {
+      scene.children[1].rotation.y += radians(3);
+      scene.children[1].children[0].rotation.x += radians(30);
+      //console.log(scene.children[1].children[0]);
+    }
 
     renderer.render(scene, camera);
     control.update();
   }
   tick();
-  /* || ループ処理設定終わり */
-};
-
-// ==========================================
-// 描画系関数群の定義
-// ==========================================
-/**
- * @function drawCircles
- * @param {number} x - 円の中心座標x
- * @param {number} y - 円の中心座標y
- * @param {number} z - 円の中心座標z
- * @param {THREE.Mesh} mesh - meshインスタンス
- * @param {number} angle - 配置する角度
- * @description 円を描く関数 boxを使って円を描く
- */
-const drawCircles = (x, y, z, mesh, angle) => {
-  const tx = 3 * Math.cos(radians(angle));
-  const ty = 3 * Math.sin(radians(angle));
-  mesh.position.x = x + tx;
-  mesh.position.y = y + ty;
 };
 
 window.addEventListener('DOMContentLoaded', () => {
---
+++ 0524/src/three_2/shape/gltfLoad.js
@@ -0,0 +1,80 @@
+//import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+
+/** gltfを読み込む関数
+ * @function gltfLoad
+ * @param {string} path - gltfのパス
+ * @param {THREE.Scene} scene - シーン
+ */
+export const gltfLoad = (path, scene, material, meshLoad_frag) => {
+  // 実装参考：https://threejs.org/docs/#examples/en/loaders/GLTFLoader
+  const loader = new GLTFLoader();
+
+  // Load a glTF resource
+  loader.load(
+    path,
+    function (gltf) {
+      // メッシュの定義
+      const blade = gltf.scene.children[0]; // new THREE.Mesh(geometry_blade, material);(個別に設定するとサイズ感も狂う)
+      const net_back = gltf.scene.children[1];
+      const net_front = gltf.scene.children[2];
+      const prop = gltf.scene.children[3];
+      const handle = gltf.scene.children[4];
+      blade.material = material; // 上書きできる
+
+      for (let i = 0; i < net_back.children.length; i++) {
+        net_back.children[i].material = material;
+      }
+
+      //   console.log(net_back, 'hello');
+      //   console.log(net_front, 'hello');
+      for (let i = 0; i < net_front.children.length; i++) {
+        net_front.children[i].material = material;
+      }
+
+      prop.material = material;
+      handle.material = material;
+      const head = new THREE.Group();
+      head.add(blade);
+      head.add(net_back);
+      head.add(net_front);
+
+      // 原点をずらすための重み付け
+      const inversion_net_back = net_back.clone();
+      const inversion_net_front = net_front.clone();
+      inversion_net_back.rotation.y = Math.PI;
+      inversion_net_back.position.x = -3.03;
+      inversion_net_front.rotation.y = Math.PI;
+      inversion_net_front.position.x = -3.75;
+      head.add(inversion_net_back);
+      head.add(inversion_net_front);
+
+      //scene.add(blade);
+      //scene.add(net_back);
+      //scene.add(net_front);
+
+      scene.add(head);
+      scene.add(prop);
+      scene.add(handle);
+
+      // gltf.scene.children[0] = プロペラ
+      // gltf.scene.children[1] = 後ろの部分
+      // gltf.scene.children[2] = 手前のフレーム部分
+      // gltf.scene.children[3] = 棒の部分
+      // gltf.scene.children[4] = ハンドルの部分
+      meshLoad_frag.bool = true;
+    },
+
+    // called while loading is progressing
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+    },
+
+    // called when loading has errors
+    function (error) {
+      console.log('An error happened');
+      return false;
+    }
+  );
+};
